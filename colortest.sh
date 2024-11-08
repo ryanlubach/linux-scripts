5cca85c9fa51de3cb3d04c28149da6f35477ae41
Ryan <138840662+ryanlubach@users.noreply.github.com>
2024-11-07 17:33:02 -0800

    Initial commit
    Added script to test terminal output colors.

---
+++ colortest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script to display bash echo output colors
+
+echo
+echo 'OPTIONS FOR echo -e "\033[#;##mTextHere" COLOR OUTPUT'
+echo
+echo -e "\033[0;31mColor test: 0 31  ██"
+echo -e "\033[0;32mColor test: 0 32  ██"
+echo -e "\033[0;33mColor test: 0 33  ██"
+echo -e "\033[0;34mColor test: 0 34  ██"
+echo -e "\033[0;35mColor test: 0 35  ██"
+echo -e "\033[0;36mColor test: 0 36  ██"
+echo -e "\033[0;37mColor test: 0 37  ██"
+echo '--------------------'
+echo -e "\033[1;30mColor test: 1 30  ██"
+echo -e "\033[1;31mColor test: 1 31  ██"
+echo -e "\033[0;32mColor test: 1 32  ██"
+echo -e "\033[0;33mColor test: 1 33  ██"
+echo -e "\033[0;34mColor test: 1 34  ██"
+echo -e "\033[0;35mColor test: 1 35  ██"
+echo -e "\033[0;36mColor test: 1 36  ██"
+echo -e "\033[0;37mColor test: 1 37  ██"
+echo
